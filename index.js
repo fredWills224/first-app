a2e50ac4ce3a0b73ee9a1511af9a1116dbf4bad0
fredWills224 <fredwills4229@gmail.com>
2021-02-08 12:58:55 -0500

    routing requests

--- index.js
+++ index.js
@@ -21,11 +21,13 @@
 
         // Get the path
             
-            // [pathname] is a key on the object [parsedUrl] 
-            // [path] = foo -> http://localhost3000/foo
-            var path = parsedUrl.pathname;
+            // [pathname] is a key on the object [parsedUrl], [path] = foo -> http://localhost3000/foo
+                var path = parsedUrl.pathname;
+            // [pathname] is a key on the object [parsedUrl], [path] = foo -> http://localhost3000/foo
+            
             // trimming path with regex inside of the replace string method
-            var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+                var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+            // trimming path with regex inside of the replace string method
         
         // Get the path
 
@@ -61,13 +63,57 @@
                 req.on('end', function(){
 
                     buffer += decoder.end();
-                    // Send the response
-                        res.end('\nHello World\n');
-                    // Send the response
 
-                    // Log the request path
-                        console.log('Request recieved with this payload: ', buffer);
-                    // Log the request path
+                    //Choose the handler this request should go to. If one is not found, use the notFound handler
+                        // If the path that the user is requesting exist as a key on the router call that handler, or call notFound handler
+                            var chosenHandler =
+                                typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound
+                            ; 
+                        // If the path that the user is requesting exist as a key on the router call that handler, or call notFound handler
+                    //Choose the handler this request should go to. If one is not found, use the notFound handler
+                    
+                    // Construct the data object to send to the handler
+                        var data = {
+                            'trimmedPath' : trimmedPath,
+                            'queryStringObject' : queryStringObject,
+                            'method' : method,
+                            'headers' : headers,
+                            'payload' : buffer
+                        };
+                    // Construct the data object to send to the handler
+                    
+                    // Route the request to the handler specified in the router
+
+                        chosenHandler(data, function(statusCode, payload){
+
+                            // Use the status code called back by the handler, or default to 200
+                                // Set [statusCode] to incoming parameter [statusCode] only if it is of type ['number'], otherwise set statusCode to the 200
+                                    statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+                                // Set [statusCode] to incoming parameter [statusCode] only if it is of type ['number'], otherwise set statusCode to the 200
+                            // Use the status code called back by the handler, or default to 200
+                            
+                            // Use the payload called back by the handler, or default to an empty object
+                                // If the typeof [buffer] is an object than set it to [payload], otherwise set [payload] to empty object 
+                                    payload = typeof(payload) == 'object' ? payload : {};
+                                // If the typeof [buffer] is an object than set it to [payload], otherwise set [payload] to empty object 
+                            // Use the payload called back by the handler, or default to an empty object
+                            
+                            // Convert the payload to a string
+                                var payloadString = JSON.stringify(payload);
+                            // Convert the payload to a string
+                            
+                            // Return the response
+                                res.writeHead(statusCode);
+                                res.end(payloadString);
+                            // Return the response
+                            
+                            // Log the request path
+                                console.log('Returning this response: ', statusCode, payloadString);
+                            // Log the request path
+                            
+                        });
+
+                    // Route the request to the handler specified in the router
                 
                 });
 
@@ -85,3 +131,31 @@
         console.log("The server is listening on port 3000 now");
     });
 // Start the server, and have it listen on port 3000
+
+// Define the handlers
+
+    var handlers = {};
+
+    // Sample handler
+        handlers.sample = function(data, callback){
+            // Callback a http status code, and a payload object 
+                callback(406, {'name': 'sample handler'});
+            // Callback a http status code, and a payload object 
+        };
+    // Sample handler
+
+    // Not found handler
+        handlers.notFound = function(data, callback){
+            // Callback a http status code
+                callback(404);
+            // Callback a http status code
+        };
+    // Not found handler
+
+// Define the handlers
+
+// Define a request router
+    var router = {
+        'sample': handlers.sample
+    };
+// Define a request router
