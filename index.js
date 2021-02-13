e5b9a642eb9418c0352b32d31ea69c2b4463d2fb
fredWills224 <fredwills4229@gmail.com>
2021-02-13 18:27:46 -0500

    note refactor

--- index.js
+++ index.js
@@ -13,19 +13,19 @@
 // Dependencies
 
 
-// Instantiate the HTTP server
+// Instantiate the [httpServer]
     var httpServer = http.createServer(function(req, res){
         unifiedServer(req,res);
     });
-// Instantiate the HTTP server
+// Instantiate the [httpServer]
 
-// Start the HTTP server
+// Start the [httpServer]
     httpServer.listen(config.httpPort, function(){
         console.log("The server is listening on port " +config.httpPort);
     });
-// Start the HTTP server
+// Start the [httpServer]
 
-// Instantiate the HTTPS server
+// Instantiate the [httpServer]
     var httpsServerOptions = {
         'key': fs.readFileSync('./https/key.pem'),
         'cert': fs.readFileSync('./https/cert.pem')
@@ -33,24 +33,24 @@
     var httpsServer = https.createServer(httpsServerOptions,function(req, res){
         unifiedServer(req, res);
     });
-// Instantiate the HTTPS server
+// Instantiate the [httpServer]
 
-// Start the HTTPS server
+// Start the [httpServer]
     httpsServer.listen(config.httpsPort, function(){
         console.log("The server is listening on port " +config.httpsPort);
     });
-// Start the HTTPS server
+// Start the [httpServer]
 
-// All the server logic for both the http and https server
+// All the server logic for both the [httpServer] and [httpsServer]
     
     var unifiedServer = function(req, res){
 
-        // Get the URL and parse it
-            //second parameter set to [true] so that the query-string-model gets called
+        // Get the [url] and parse it
+            //second parameter set to [true] so that the query-string-module gets called
             var parsedUrl = url.parse(req.url, true) 
-        // Get the URL and parse it
+        // Get the [url] and parse it
 
-        // Get the path
+        // Get the [path]
 
             // [pathname] is a key on the object [parsedUrl], [path] = foo -> http://localhost3000/foo
                 var path = parsedUrl.pathname;
@@ -60,47 +60,48 @@
                 var trimmedPath = path.replace(/^\/+|\/+$/g,'');
             // trimming path with regex inside of the replace string method
         
-        // Get the path
+        // Get the [path]
         
         // Get the query string as an object
             var queryStringObject = parsedUrl.query;
         // Get the query string as an object
         
-        // Get the HTTP Method
-            // [req] is a parameter passed to the [server] function
+        // Get the HTTP [method]
+            // [req] is a parameter passed to the createServer function
             var method = req.method.toLocaleLowerCase();
-        // Get the HTTP Method
+        // Get the HTTP [method]
         
-        // Get the headers as an object
+        // Get the [headers] as an object
             var headers = req.headers;
-        // Get the headers as an object
+        // Get the [headers] as an object
         
-        // Get the payload, if any 
+        // Get the [payload], if any 
             
-            // decode and append incoming data to empty string
+            // decode and append incoming [data] to empty string
+
                 var decoder = new StringDecoder('utf-8');
-                var buffer = '';    
-            // decode and append incoming data to empty string
+                var buffer = '';                
+                // [req]'s [data] event handler
+                    req.on('data', function(data){
+                        buffer += decoder.write(data);
+                    });
+                // [req]'s [data] event handler
             
-            // binding to event that the [req] object emits called data
-                req.on('data', function(data){
-                    buffer += decoder.write(data);
-                });
-            // binding to event that the [req] object emits called data
-            
+            // decode and append incoming [data] to empty string
+
             // [req]'s end event handler
                 req.on('end', function(){
                     
                     buffer += decoder.end();            
-                    //Choose the handler this request should go to. If one is not found, use the notFound handler
-                        // If the path that the user is requesting exist as a key on the router call that handler, or call notFound handler
+                    //Choose the handler this request should go to. If one is not found, use the [notFound] handler
+                        // If the [path] that the user is requesting exist as a key on the [router] call that handler, or call [notFound] handler
                             var chosenHandler =
                                 typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound
                             ; 
-                        // If the path that the user is requesting exist as a key on the router call that handler, or call notFound handler
-                    //Choose the handler this request should go to. If one is not found, use the notFound handler
+                        // If the [path] that the user is requesting exist as a key on the [router] call that handler, or call [notFound] handler
+                    //Choose the handler this request should go to. If one is not found, use the [notFound] handler
                     
-                    // Construct the data object to send to the handler
+                    // Construct the [data] object to send to the handler
                         var data = {
                             'trimmedPath' : trimmedPath,
                             'queryStringObject' : queryStringObject,
@@ -108,71 +109,70 @@
                             'headers' : headers,
                             'payload' : buffer
                         };
-                    // Construct the data object to send to the handler
+                    // Construct the [data] object to send to the handler
                     
-                    // Route the request to the handler specified in the router
+                    // Route the request to the handler specified in [router]
                         chosenHandler(data, function(statusCode, payload){
                             
-                            // Use the status code called back by the handler, or default to 200
-                                // Set [statusCode] to incoming parameter [statusCode] only if it is of type ['number'], otherwise set statusCode to the 200
+                            // Use the [statusCode] called back by the handler, or default to 200
+                                // Set [statusCode] to incoming parameter [statusCode] only if it is of type ['number'], otherwise set [statusCode] to the 200
                                     statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-                                // Set [statusCode] to incoming parameter [statusCode] only if it is of type ['number'], otherwise set statusCode to the 200
-                            // Use the status code called back by the handler, or default to 200
+                                // Set [statusCode] to incoming parameter [statusCode] only if it is of type ['number'], otherwise set [statusCode] to the 200
+                            // Use the [statusCode] called back by the handler, or default to 200
                             
-                            // Use the payload called back by the handler, or default to an empty object
+                            // Use the [payload] called back by the handler, or default to an empty object
                                 // If the typeof [buffer] is an object than set it to [payload], otherwise set [payload] to empty object 
                                     payload = typeof(payload) == 'object' ? payload : {};
                                 // If the typeof [buffer] is an object than set it to [payload], otherwise set [payload] to empty object 
-                            // Use the payload called back by the handler, or default to an empty object
+                            // Use the [payload] called back by the handler, or default to an empty object
                             
-                            // Convert the payload to a string
+                            // Convert the [payload] to a string
                                 var payloadString = JSON.stringify(payload);
-                            // Convert the payload to a string
+                            // Convert the [payload] to a string
                             
-                            // Return the response
+                            // Return the response [res]
                                 res.setHeader('Content-Type', 'application/json');
                                 res.writeHead(statusCode);
                                 res.end(payloadString);
-                            // Return the response
+                            // Return the response [res]
                             
-                            // Log the request path
+                            // Log the request [path]
                                 console.log('Returning this response: ', statusCode, payloadString);
-                            // Log the request path
+                            // Log the request [path]
                         
                         });
-                    // Route the request to the handler specified in the router
+                    // Route the request to the handler specified in [router]
                     
                 });
             // [req]'s end event handler
             
-        // Get the payload, if any
+        // Get the [payload], if any
+
+        // Define the [handlers]
+            var handlers = {};
+                    
+            // [ping] handler
+                handlers.ping = function(data, callback){
+                    callback(200);
+                };
+            // [ping] handler
+            
+            // [notFound] handler
+                handlers.notFound = function(data, callback){
+                    // Callback a http status code
+                        callback(404);
+                    // Callback a http status code
+                };
+            // [notFound] handler
+            
+        // Define the [handlers]
+            
+        // Define a request [router]
+            var router = {
+                'ping': handlers.ping
+            };
+        // Define a request [router]
 
     };
 
-// All the server logic for both the http and https server
-
-// Define the handlers
-
-    var handlers = {};
-
-    // Ping handler
-        handlers.ping = function(data, callback){
-            callback(200);
-        };
-    // Ping handler
-
-    // Not found handler
-        handlers.notFound = function(data, callback){
-            // Callback a http status code
-                callback(404);
-            // Callback a http status code
-        };
-    // Not found handler
-
-// Define the handlers
-
-// Define a request router
-    var router = {
-        'ping': handlers.ping
-    };
-// Define a request router
+// All the server logic for both the [httpServer] and [httpsServer]
