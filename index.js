139b13e2142c4d0a3ae1ad63d0308cb099805e8a
fredWills224 <fredwills4229@gmail.com>
2021-02-16 20:55:36 -0500

    handlers._tokens.post + helpers.createRandomString

--- index.js
+++ index.js
@@ -180,7 +180,8 @@
         // Define a request [router]
             var router = {
                 'ping' : handlers.ping,
-                'users' : handlers.users
+                'users' : handlers.users,
+                'tokens' : handlers.tokens
             };
         // Define a request [router]
 
